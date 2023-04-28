5089406dc7755661cccab2ef678ea4d06ab7d8bd
jasminericka <jasmin.celebre@gmail.com>
2023-04-28 18:30:42 +0800

    pseudocode

--- program1.py
+++ program1.py
@@ -1,2 +1,11 @@
 #jasmin ericka a. celebre
 #bscpe 1-4
+
+
+#open the numbers.txt file
+with open ("numbers.txt", "r") as my_file:
+#read the content of the numbers.txt file
+    numbers = my_file.read()
+    numbers_list = numbers.split()
+#create the even.txt file
+#create the odd.txt file
